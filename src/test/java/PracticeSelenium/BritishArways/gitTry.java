660870389a7d5de8248c508619d452660fe44c4c
Sachi2511 <sachi.kayastha@gmail.com>
2025-11-28 12:46:18 +0000

    brach checkout

---
+++ src/test/java/PracticeSelenium/BritishArways/gitTry.java
@@ -0,0 +1,12 @@
+package PracticeSelenium.BritishArways;
+
+import org.testng.annotations.Test;
+
+public class gitTry {
+
+	@Test
+	public void tryforgit()
+	{
+		System.out.println("trying to work with git");
+	}
+}
